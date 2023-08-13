359cc0783f926fe46ed104781b2afc964eb6e01a
Harsh <harshgurjar1908@gmail.com>
2023-08-13 12:17:23 +0530

    first change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  
+  let [num,setNum]=useState(0);
+
+  const inc=()=>{
+    num++;
+    setNum(num);
+  }
+  
+  const dec=()=>{
+  num--;
+  if(num<=-1){
+    alert("you have reached zero");
+    setNum(0);
+  }
+  else{
+  setNum(num);
+  }
+}
+
+  
+  return(
+<>
+<div className="main">
+  <h1>{num}</h1>
+  <br/><br/>
+  <button className="incbutton" onClick={inc}>Increment</button>
+  <button className="decbutton" onClick={dec}>Decrement</button>
+
+</div>
+</>
   );
 }
 
